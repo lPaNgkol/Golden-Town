9faa60259ba69229d2caa85bf5ac1cb959f8ce31
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-04-29 01:57:31 +0000

    Update department.js

--- app/models/department/department.js
+++ app/models/department/department.js
@@ -63,23 +63,23 @@ function createDepartment(req, res) {
       });
     }
 
+    
 function updateDepartment(req, res) {
   var time = moment();
-  var date = time.format('YYYY-MM-DDTHH:mm:ss');
-  aDatetime = String(date).split("T")
-  var dateNow = aDatetime[0] + " " + aDatetime[1]
+  var date = time.format("YYYY-MM-DDTHH:mm:ss");
+  aDatetime = String(date).split("T");
+  var dateNow = aDatetime[0] + " " + aDatetime[1];
   const department_name = req.body.departmentname;
-
+  const createby = req.body.createby;
   const updateby = req.body.updateby;
-  const updatedate = dateNow;
+
   const department_id = req.params.department_id;
 
   return new Promise(function (resolve) {
-    const query = `UPDATE department
-                     SET department_name=$1, updateby=$2, updatedate=$3,
-                     WHERE UPPER(department_id) = UPPER($4) 
-                     RETURNING department_id;`;
-    const dataquery = [department_name, updateby, updatedate, department_id];
+    const query = `UPDATE department SET department_name=$1, createby=$2, updateby=$3
+                     WHERE(department_id)=$4
+                     RETURNING department_id`;
+    const dataquery = [department_name, createby, updateby, department_id];
     db.query(query, dataquery)
       .then((results) => {
         console.log(results.rows);
@@ -94,17 +94,16 @@ function updateDepartment(req, res) {
 }
 
 function deleteDepartment(req, res) {
-  return new Promise(async (resolve) => {
-    try {
-      let deleteDepart = req.param.department_id;
-      const data = await db.query(`DELETE FROM department WHERE department_id = ${deleteDepart}`, [deleteDepart]);
-      let results = data.rows;
-    }
-    catch (error) {
-      console.error("### Error ", error);
-      return resolve(error);
-    }
-  });
+return new Promise(async (resolve) => {
+try {
+  const result = await db.query("DELETE FROM department WHERE department_id=$1", [
+    req.params.department_id
+  ]);
+  return res.json({ message: "Deleted" });
+} catch (err) {
+  return (err, "error");
+}
+});
 }
 
 const department = {
