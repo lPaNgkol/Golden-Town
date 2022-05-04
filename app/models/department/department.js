f2e9aee690e186490117dd0b93c370dac605d7e9
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-05-04 15:58:33 +0000

    config error change get by company id

--- app/models/department/department.js
+++ app/models/department/department.js
@@ -1,7 +1,6 @@
 const db = require("../dbconnection");
 var format = require("pg-format");
 var moment = require("moment");
-const { render } = require("express/lib/response");
 
 // get all
 function departmentList(req, res) {
@@ -15,7 +14,10 @@ function departmentList(req, res) {
 
       return resolve(results);
     } catch (error) {
-      res.status(500).send({
+      console.error("### Error ", error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
         description: error.message,
       });
     }
@@ -23,7 +25,7 @@ function departmentList(req, res) {
 }
 
 // get by companyId
-function departmentById(req, res) {
+function departmentByCompanyId(req, res) {
   return new Promise(async (resolve) => {
     try {
       let companyId = req.params.company_id;
@@ -34,33 +36,61 @@ function departmentById(req, res) {
       let results = data.rows;
       return resolve(results, results.length);
     } catch (error) {
-      console.error(error);
-      return resolve(error);
+      console.error("### Error ", error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
+        description: error.message,
+      });
+    }
+  });
+}
+
+//gat by department_id
+function departmentBydepartmentId(req, res) {
+  return new Promise(async (resolve) => {
+    try {
+      let department_id = req.params.department_id;
+      const data = await db.query(
+        `SELECT department_id, department_name FROM department WHERE department_id = $1`,
+        [department_id]
+      );
+      let results = data.rows;
+      return resolve(results, results.length);
+    } catch (error) {
+      console.error("### Error ", error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
+        description: error.message,
+      });
     }
   });
 }
 
 // create
 function createDepartment(req, res) {
-  return new Promise(function (resolve) {
+  return new Promise(async function (resolve) {
     try {
-      var time = moment();
-      var date = time.format("YYYY-MM-DDTHH:mm:ss");
-      aDatetime = String(date).split("T");
-      var dateNow = aDatetime[0] + " " + aDatetime[1];
-      const department_id = req.body.department_id;
+      const dateNow = moment().format("YYYY-MM-DD HH:mm:ss");
+      // const department_id = req.body.department_id;
+      // console.log("req", req)
       const department_name = req.body.department_name;
-      const company_id = req.body.company_id;
-      const createby = req.body.createby;
-      const updateby = req.body.updateby;
+      const company_id = req.params.company_id;
+      const createby = req.user_id;
+      const updateby = req.user_id;
       const createdate = dateNow;
       const updatedate = dateNow;
-      const data = db.query(
+      const data = await db.query(
         `INSERT INTO department(department_name, company_id, createby, updateby, createdate, updatedate)
-              VALUES ($1, 1, 1, 1, current_timestamp, current_timestamp) RETURNING department_id`,
+              VALUES ($1, $2, $3, $4, $5, $6) RETURNING department_id`,
         [
           department_name,
-
+          company_id,
+          createby,
+          updateby,
+          createdate,
+          updatedate,
         ]
       );
 
@@ -68,40 +98,43 @@ function createDepartment(req, res) {
       return resolve(results);
     } catch (error) {
       console.error("### Error ", error);
-      return resolve(error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
+        description: error.message,
+      });
     }
   });
 }
 
 //update
 function updateDepartment(req, res) {
-  return new Promise(function (resolve) {
+  return new Promise(async function (resolve) {
     try {
-      var time = moment();
-      var date = time.format("YYYY-MM-DDTHH:mm:ss");
-      aDatetime = String(date).split("T");
-      var dateNow = aDatetime[0] + " " + aDatetime[1];
+      const dateNow = moment().format("YYYY-MM-DD HH:mm:ss");
       const department_name = req.body.department_name;
       const company_id = req.body.company_id;
-      const createby = req.body.createby;
-      const updateby = req.body.updateby;
+      const createby = req.user_id;
+      const updateby = req.user_id;
       const createdate = dateNow;
       const updatedate = dateNow;
       const department_id = req.params.department_id;
-      const data = db.query(
-        `UPDATE department SET department_name=$1, updateby=1, updatedate=current_timestamp
+      const data = await db.query(
+        `UPDATE department SET department_name=$1, updateby=$3, updatedate=$4
                      WHERE department_id=$2
                      RETURNING department_id;`,
-        [
-          department_name,
-          department_id
-        ]
+        [department_name, department_id, updateby, updatedate]
       );
-      let results = data.rows;
+      // console.log("dd", department_name,department_id,updateby,updatedate);
+      let results = data.rowCount != 0 ? data.rows[0] : false;
       return resolve(results);
     } catch (error) {
       console.error("### Error ", error);
-      return resolve(error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
+        description: error.message,
+      });
     }
   });
 }
@@ -109,22 +142,31 @@ function updateDepartment(req, res) {
 //delete
 function deleteDepartment(req, res) {
   return new Promise(async (resolve) => {
+
     try {
       const result = await db.query(
         "DELETE FROM department WHERE department_id=$1",
         [req.params.department_id]
       );
+ 
       let results = result.rows;
-      return resolve(results, "Deleted success");
-    } catch (err) {
-      return err, "error";
+      // console.log("testresults", reeS.length);
+      return resolve(results, results.length);
+    } catch (error) {
+      console.error("### Error ", error);
+      // return resolve(false);
+      return res.status(500).send({
+        code: "WEAT500",
+        description: error.message,
+      });
     }
   });
 }
 
 const department = {
   departmentList: departmentList,
-  departmentById: departmentById,
+  departmentByCompanyId: departmentByCompanyId,
+  departmentBydepartmentId: departmentBydepartmentId,
   createDepartment: createDepartment,
   updateDepartment: updateDepartment,
   deleteDepartment: deleteDepartment,
