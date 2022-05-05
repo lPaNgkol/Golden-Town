78d454b1cc3a7b6b26c5860828da8359824a2b6f
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-05-05 07:29:09 +0000

    change return code and error

--- app/controllers/department/department.controllers.js
+++ app/controllers/department/department.controllers.js
@@ -8,10 +8,10 @@ exports.departmentList = async (req, res) => {
   listDepartment = await department.departmentList(req, res);
   console.log(listDepartment);
   if (listDepartment.length == 0) {
-    res.status(404).send({ message: "Department Not found." });
+    res.status(200).send({ code: "WEDP404", description: "Department Not found." });
   }
   if (!authJwt) {
-    res.status(401).send({ message: "Access Token Expired" });
+    res.status(200).send({ code: "WEDP401", description: "Access Token Expired" });
   } else {
     res.status(200).send({
       total: listDepartment.length,
@@ -25,10 +25,10 @@ exports.departmentById = async (req, res) => {
   DepartmentId = await department.departmentByCompanyId(req, res);
   console.log(DepartmentId.length, DepartmentId);
   if (DepartmentId.length == 0) {
-    res.status(404).send({ message: "Department Not found." });
+    res.status(200).send({ code: "WEDP404", description: "Department Not found." });
   }
   if (!authJwt) {
-    res.status(401).send({ message: "Access Token Expired" });
+    res.status(200).send({ code: "WEDP401", description: "Access Token Expired" });
   } else {
     res
       .status(200)
@@ -42,10 +42,10 @@ exports.departmentBydId = async (req, res) => {
   DepartmentId = await department.departmentBydepartmentId(req, res);
   console.log(DepartmentId.length);
   if (DepartmentId.length == 0) {
-    res.status(404).send({ message: "Department Not found." });
+    res.status(200).send({ code: "WEDP404", description: "Department Not found." });
   }
   if (!authJwt) {
-    res.status(401).send({ message: "Access Token Expired" });
+    res.status(200).send({ code: "WEDP401", description: "Access Token Expired" });
   } else {
     res
       .status(200)
@@ -59,16 +59,20 @@ exports.createDepartment = async (req, res) => {
   if (!req.body.department_name) {
     res
       .status(400)
-      .send({ Code: "400", message: "department_name cannot Be Null." });
+      .send({ Code: "WEDP400", description: "Department_name cannot Be Null." });
   }
   departmentData = await department.createDepartment(req, res);
   if (req.body.department_id) {
-    res.status(404).send({ Code: "404", message: "Department Id not Found." });
+    res
+      .status(200)
+      .send({ code: "WEDP404", description: "Department not Found." });
   }
   if (!authJwt) {
-    res.status(401).send({ code: "401", message: "Access Token Expired" });
+    res
+      .status(200)
+      .send({ code: "WEDP401", description: "Access Token Expired" });
   } else {
-    res.status(200).send({ code: "200", massage: "Success" });
+    res.status(200).send({ code: "WEDP200", description: "Success" });
     // department: departmentData
   }
 };
@@ -78,30 +82,35 @@ exports.updateDepartment = async (req, res) => {
   try {
     var departmentData = null;
     if (!req.body.department_name) {
-      res.status(400).send({ message: "department_name cannot Be Null." });
+      res.status(400).send({
+        code: "WEDP400",
+        description: "Departmentname cannot Be Null.",
+      });
     }
     if (!authJwt) {
-      res.status(401).send({ message: "Access Token Expired" });
+      res
+        .status(200)
+        .send({ code: "WEDP401", description: "Access Token Expired" });
     }
     departmentData = await department.updateDepartment(req, res);
     // console.log("departmentData", departmentData);
 
     if (!departmentData) {
       res
-        .status(404)
-        .send({ code: "404", message: "Department Id not Found." });
+        .status(200)
+        .send({ code: "WEDP404", description: "Department not Found." });
     }
 
-    res.status(200).send({ code: "200", massage: "Success" });
+    res.status(200).send({ code: "WEDP200", description: "Success" });
 
     // if(!departmentData) {
-    //   res.status(500).send({ Code: "500", massage: "Error" })
+    //   res.status(500).send({ Code: "500", description: "Error" })
     // } else {
     //   if (departmentData.hasOwnProperty('department_id')) {
     //     if (departmentData.department_id != 0) {
-    //       res.status(200).send({ Code: "200", massage: "Success" })
+    //       res.status(200).send({ Code: "WEDP200", description: "Success" })
     //     } else {
-    //       res.status(404).send({ Code: "404", message: "Department Id not Found." });
+    //       res.status(404).send({ Code: "WEDP404", message: "Department Id not Found." });
     //     }
     //   }
     // }
@@ -117,15 +126,21 @@ exports.deleteDepartment = async (req, res) => {
     let departmentData = "";
     departmentCk = await department.departmentBydepartmentId(req, res);
     if (!authJwt) {
-      res.status(401).send({ code: "401", message: "Access Token Expired" });
+      res
+        .status(200)
+        .send({ code: "WEDP401", description: "Access Token Expired" });
     }
-    console.log("ss test", departmentCk.length);
+    // console.log("ss test", departmentCk.length);
     if (departmentCk.length == 0) {
-      res.status(404).send({ code: "404", message: "department_id not Found." });
+      res
+        .status(200)
+        .send({ code: "WEDP404", description: "Department not Found." });
     }
     if (!departmentCk.length == 0) {
       departmentData = await department.deleteDepartment(req, res);
-      res.status(200).send({ code: "200", message: "Success" });
+      res
+        .status(200)
+        .send({ code: "WEDP404", description: "Department not Found." });
     }
     // console.log("ss testss", departmentData.length);
   } catch (error) {
