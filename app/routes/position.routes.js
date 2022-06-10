015ddb2e5bdaf65ed91c3b16d00c1fdb9d57a02e
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-06-10 06:09:41 +0000

    Update position.routes.js

--- app/routes/position.routes.js
+++ app/routes/position.routes.js
@@ -1,24 +1,36 @@
 const authJwt = require("../models/user/authentication");
 const controller = require("../controllers/position/position.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-  
-app.get(
-    "/position",
+
+  app.get("/position", [authJwt.verifyToken], controller.positionList);
+
+  app.get("/position/:company_id", [authJwt.verifyToken], controller.positioncompanyList);
+
+  app.post(
+    "/position/:company_id",
     [authJwt.verifyToken],
-    controller.positionList
+    controller.positionnameCheck,
+    controller.positioncompanyCheck,
+    controller.createPosition
   );
 
   app.post(
-    "/position",
-    [authJwt.verifyToken], 
-    controller.createPosition
+    "/position/id/:position_id",
+    [authJwt.verifyToken],
+    controller.updatePosition
+  );
+
+  app.delete(
+    "/position/id/:position_id",
+    [authJwt.verifyToken],
+    controller.deletePosition
   );
 };
