1e17b56fb10300c0cf610010fd5c5e5d1ae16577
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-06-20 03:09:23 +0000

    Update healthinfo.routes.js

--- app/routes/healthinfo.routes.js
+++ app/routes/healthinfo.routes.js
@@ -1,22 +1,35 @@
 const authJwt = require("../models/user/authentication");
 const controller = require("../controllers/healthinfo/healthinfo.controller");
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
+  app.get("/health", [authJwt.verifyToken], controller.healthinfoList);
   app.get(
-    "/health",
+    "/health/:user_id",
     [authJwt.verifyToken],
-    controller.healthinfoList
+    controller.healthinfoByuserId
+  );
+  app.post(
+    "/health/:user_id",
+    [authJwt.verifyToken],
+    controller.useridCheck,
+    controller.createHealthinfo
   );
 
-  app.post(
-    "/health",
-    [authJwt.verifyToken], 
-    controller.createHealthinfo
+  app.put(
+    "/health/:healthinfo_id/:user_id",
+    [authJwt.verifyToken],
+    controller.updateHealthinfo
+  );
+
+  app.delete(
+    "/health/:healthinfo_id",
+    [authJwt.verifyToken],
+    controller.deleteHealthinfo
   );
 };
