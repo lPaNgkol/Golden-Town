30909ddeca470af28908db63c4c7af7d90be02ec
pongsiri kamkongkaew <pongsiri.k@spacecloudtech.com>
2022-06-24 08:07:34 +0000

    Update healthinfo.controller.js

--- app/controllers/healthinfo/healthinfo.controller.js
+++ app/controllers/healthinfo/healthinfo.controller.js
@@ -6,38 +6,47 @@ exports.healthinfoList = async (req, res) => {
   listHealthinfo = await healthinfo.healthinfoList(req, res);
   console.log(listHealthinfo);
   if (listHealthinfo.length == 0) {
-    res.status(200).send({ code: "WEHI401", description: "Healthinfo Id Not found." });
+    res
+      .status(200)
+      .send({ code: "WEHI401", description: "Healthinfo Id Not found." });
   } else {
     res.status(200).send({
       totalRow: listHealthinfo[0].total_row,
       ListHealthinfo: listHealthinfo,
-
     });
   }
 };
 
 exports.healthinfoByuserId = async (req, res) => {
-    var listHealthinfo = "";
-    listHealthinfo = await healthinfo.healthinfoByuserId(req, res);
-    console.log(listHealthinfo);
-    if (listHealthinfo.length == 0) {
-      res.status(200).send({ code: "WEHI401", description: "Healthinfo Id Not found." });
-    } else {
-      res.status(200).send({totalRow: listHealthinfo[0].total_row,
-       ListHealthinfo: listHealthinfo,
-        
+  var listHealthinfo = "";
+  listHealthinfo = await healthinfo.healthinfoByuserId(req, res);
+  console.log(listHealthinfo);
+  if (listHealthinfo.length == 0) {
+    res
+      .status(200)
+      .send({ code: "WEHI401", description: "Healthinfo Id Not found." });
+  } else {
+    res
+      .status(200)
+      .send({
+        totalRow: listHealthinfo[0].total_row,
+        ListHealthinfo: listHealthinfo,
       });
-    }
-  };
+  }
+};
 
 exports.createHealthinfo = async (req, res) => {
   var healthinfoData = "";
   if (!req.params.user_id) {
-    res.status(200).send({ code: "WEHI400", description: "user_id cannot Be Null." });
+    res
+      .status(200)
+      .send({ code: "WEHI400", description: "user_id cannot Be Null." });
   }
   healthinfoData = await healthinfo.createHealthinfo(req, res);
   if (healthinfoData.length == 0) {
-    res.status(200).send({ code: "WEHI404", description: "healthinfo Id Not found." });
+    res
+      .status(200)
+      .send({ code: "WEHI404", description: "healthinfo Id Not found." });
   } else {
     res.status(200).send({ code: "WEHI200", description: "Success" });
   }
@@ -64,8 +73,9 @@ exports.updateHealthinfo = async (req, res) => {
         .status(403)
         .send({ code: "WEHI403", description: "Health Info ID Not Found." });
     } else {
-      res.status(200).send({ 
-        code: "WEHI200", description: "Success"
+      res.status(200).send({
+        code: "WEHI200",
+        description: "Success",
       });
     }
   } catch (error) {
@@ -74,32 +84,28 @@ exports.updateHealthinfo = async (req, res) => {
   }
 };
 
-
 exports.deleteHealthinfo = async (req, res) => {
   try {
     var healthinfoData = "";
-    if (!authJwt) {
+    if (!req.params.healthinfo_id) {
       res
         .status(200)
-        .send({ code: "WEHI401", description: "Access Token Expired" });
+        .send({ code: "WEHI400", description: "Healthinfo  id cannot be Null." });
     }
     healthinfoData = await healthinfo.deleteHealthinfo(req, res);
-    if (healthinfoData.length == 0) {
+    if (healthinfoData != "complete") {
       res
-        .status(200)
-        .send({ code: "WEHI404", description: "healthinfo Id Not found." });
+        .status(404)
+        .send({ code: "WEHI404", description: "Healthinfo Id Not found." });
     }
     if (!healthinfoData.length == 0) {
-      res
-        .status(200)
-        .send({ code: "WEHI200", description: "healthinfo Id Not found." });
+      res.status(200).send({ code: "WEHI200", description: "Delete Complete!" });
     }
   } catch (error) {
     console.error("### Error ", error);
     return error;
   }
 };
-
 
 exports.useridCheck = async (req, res, next) => {
   if (!authJwt) {
@@ -109,11 +115,9 @@ exports.useridCheck = async (req, res, next) => {
   }
   useridCheck = await healthinfo.useridCheck(req, res, next);
   if (useridCheck == 0) {
-    res
-      .status(200)
-      .send({ code: "WEHI404", description: "User ID Not Found" });
-  } if (useridCheck == 1) {
-
+    res.status(200).send({ code: "WEHI404", description: "User ID Not Found" });
+  }
+  if (useridCheck == 1) {
     next();
   }
-}
+};
